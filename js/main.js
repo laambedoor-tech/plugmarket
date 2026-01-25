dfc26fe8957343b76744bb04316a27342e1dcb1b
laambedoor-tech <laambedoor@gmail.com>
2026-01-26 00:11:54 +0100

    Add YouTube Premium Family Owner Lifetime FA variant at €4.74

--- js/main.js
+++ js/main.js
@@ -177,7 +177,7 @@ const subscriptions = {
 
   // Ajustados tomando como guía las capturas (valores diferenciados y sin Lifetime)
   spotify: { '1 Month': 1.98, '3 Months': 3.42, '6 Months': 5.58, '12 Months': 10.69, 'Lifetime': 1.78 },
-  'youtube-premium': { '1 Month': 2.52, '3 Months': 3.89, '12 Months': 11.25, 'Lifetime': 2.27 },
+  'youtube-premium': { '1 Month': 2.52, '3 Months': 3.89, '12 Months': 11.25, 'Lifetime': 2.27, 'fowner': 4.74 },
   disney: { '1 Month': 0.89, '3 Months': 2.43, '6 Months': 4.32, '12 Months': 8.55 },
   prime: { '1 Month': 1.62, '3 Months': 3.06, '6 Months': 5.22, '12 Months': 9.58 },
   hbomax: { '1 Month': 1.08, '3 Months': 2.7, '6 Months': 4.14, '12 Months': 6.75 },
